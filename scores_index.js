bde7de8b3b5e1fc93ed2a1e7ce2b0abd7f6e98a9
Kylie S <kyliemegan24@gmail.com>
2020-09-29 17:06:05 -0500

    Link issues

--- scores_index.js
+++ scores_index.js
@@ -16,7 +16,7 @@ clear.addEventListener("click", function(event) {
 
     if (confirm) {
         localStorage.removeItem("highScores")
-        window.location.href = "https://kyliemegan24.github.io/MandMs_PopQuiz/"
+        window.location.href = "index.html"
     }
 
 })
